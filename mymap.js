0b42ad0471e3c1bbfa4091e6a502398c85201d42
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-10-29 12:29:02 +0100

    This is a test master

---
+++ mymap.js
@@ -0,0 +1,34 @@
+var osm = L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         maxZoom: 18,
+         attribution: 'Map data © \
+              <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+        });
+
+var ortho = L.tileLayer.wms(
+         'https://www.wms.nrw.de/geobasis/wms_nw_dop?', {
+         layers: 'nw_dop_rgb'
+       });
+
+var counties = new L.GeoJSON.AJAX("counties.geojson", {
+          style: {"color": "#ff7800",
+                  "weight": 5,
+                  "opacity": 0.65},
+          onEachFeature: function(feature, layer){
+              layer.bindPopup(feature.properties.GN);
+          }
+       });
+
+var mymap = L.map('map', {
+    center: [50.938056, 6.956944],
+    zoom: 8,
+    layers: [osm, ortho, counties] // add it here
+});
+
+L.control.scale().addTo(mymap);
+
+var basemaps = {"OpenStreetMap": osm}
+var overlays = {"Orthophoto": ortho,
+                "County borders": counties}
+
+L.control.layers(basemaps, overlays).addTo(mymap);
