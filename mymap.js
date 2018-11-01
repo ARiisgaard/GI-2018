30130a8b063e4445e801c855e387cdeb73affdfa
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-11-01 11:02:29 +0100

    Har ryddet lidt op og flyttet centeret til KBH

--- mymap.js
+++ mymap.js
@@ -5,24 +5,10 @@ var osm = L.tileLayer(
               <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
         });
 
-var ortho = L.tileLayer.wms(
-         'https://www.wms.nrw.de/geobasis/wms_nw_dop?', {
-         layers: 'nw_dop_rgb'
-       });
-
-var counties = new L.GeoJSON.AJAX("counties.geojson", {
-          style: {"color": "#ff7800",
-                  "weight": 5,
-                  "opacity": 0.65},
-          onEachFeature: function(feature, layer){
-              layer.bindPopup(feature.properties.GN);
-          }
-       });//Dette er en test
-
 var mymap = L.map('map', {
-    center: [50.938056, 6.956944],
-    zoom: 8,
-    layers: [osm, ortho, counties] // add it here
+    center: [55.676111, 12.568333],
+    zoom: 10,
+    layers: [osm] // add it here
 });
 
 L.control.scale().addTo(mymap);//Dette er en test
