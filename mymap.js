04064b7f677b5923017541f76ff8bb944f5c1711
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-11-01 11:15:58 +0100

    New branch for tests with OSRM

--- mymap.js
+++ mymap.js
@@ -5,16 +5,25 @@ var osm = L.tileLayer(
               <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
         });
 
+        var firstpolyline = new L.Polyline(pointList, {
+            color: 'red',
+            weight: 3,
+            opacity: 0.5,
+            smoothFactor: 1
+        });
+
 var mymap = L.map('map', {
     center: [55.676111, 12.568333],
     zoom: 10,
-    layers: [osm] // add it here
+    layers: [osm, firstpolyline] // add it here
 });
 
-L.control.scale().addTo(mymap);//Dette er en test
+L.control.scale().addTo(mymap);
 
 var basemaps = {"OpenStreetMap": osm}
 var overlays = {"Orthophoto": ortho,
                 "County borders": counties}
 
 L.control.layers(basemaps, overlays).addTo(mymap);
+
+get /route/v1/bike/55.650575, 12.541276;55.678437, 12.572282?alternatives=2&steps=false&geometries=polyline&overview=full&annotations=false
