086b3eaf7e040dca33f250a87603a84c457a44b0
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-11-05 09:50:24 +0100

    Further comments

--- mymap.js
+++ mymap.js
@@ -17,7 +17,7 @@ L.control.scale().addTo(mymap);
 var basemaps = {"OpenStreetMap": osm}
  var overlays = {"Route": control}
 
-var StartLocation = L.latLng(55.650575, 12.541276)
+var StartLocation = L.latLng(55.650575, 12.541276) //The start of the journey - is later going to be changed to gps coordinates
 var EndLocation = L.latLng(55.678437, 12.572282)
 
 var control = L.Routing.control({
@@ -27,7 +27,7 @@ EndLocation
   ],
   router: new L.Routing.osrmv1({
 serviceUrl: "https://router.project-osrm.org/route/v1",
-    language: 'en', 
+    language: 'en',
     profile: 'bike', //Method of transport
     steps: 'true' //Adds a guide for the trip
   })//,
