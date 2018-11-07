c58dd87ccdb97faec42e4640246439eca09f0a78
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-11-07 14:31:49 +0100

    Added gps to js

--- mymap.js
+++ mymap.js
@@ -5,6 +5,25 @@ var osm = L.tileLayer(
               <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
   });
 
+  map.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
+          .on('locationfound', function(e){
+              var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
+              var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+                  weight: 1,
+                  color: 'blue',
+                  fillColor: '#cacaca',
+                  fillOpacity: 0.2
+              });
+              map.addLayer(marker);
+              map.addLayer(circle);
+          })
+         .on('locationerror', function(e){
+              console.log(e);
+              alert("Location access denied.");
+          });
+  
+
+
 var StartLocation = L.latLng(55.650575, 12.541276) //The start of the journey - is later going to be changed to gps coordinates
 var EndLocation = L.latLng(55.678437, 12.572282)
 
@@ -52,3 +71,41 @@ L.control.scale().addTo(mymap);
 // })
 //mymap.getCenter()
 //
+
+
+// mymap.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
+//     .on('locationfound', function(e){
+//         var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
+//         var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+//             weight: 1,
+//             color: 'blue',
+//             fillColor: '#cacaca',
+//             fillOpacity: 0.2
+//         });
+//         mymap.addLayer(marker);
+//         mymap.addLayer(circle);
+//         StartCoordinates = [e.latitude, e.longitude]
+//
+//         var StartLocation = StartCoordinates//L.latLng(55.650575, 12.541276) //The start of the journey - is later going to be changed to gps coordinates
+//         var EndLocation = L.latLng(55.678437, 12.572282)
+//         L.Routing.control({
+//           waypoints: [
+//         StartLocation,
+//         EndLocation
+//           ],
+//           router: new L.Routing.osrmv1({
+//             language: 'en',
+//             profile: 'bike', //Method of transport
+//             steps: 'true' //Adds a guide for the trip
+//           })//,
+//         //  geocoder: L.Control.Geocoder.nominatim({}) This code I haven't activated yet, but it should help translating from addresses to latlon
+//         })
+//         //})
+//         .addTo(mymap);
+//
+//     })
+//    .on('locationerror', function(e){
+//         console.log(e);
+//         alert("Location access denied.");
+//     });;
+//     alert(String(StartCoordinates));
