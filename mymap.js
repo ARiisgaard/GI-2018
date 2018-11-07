abad6e181d2fe458525f3c0144061fa8e776802b
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-11-07 12:19:34 +0100

    Routeplaner with ORS - And it works!

--- mymap.js
+++ mymap.js
@@ -1,16 +1,35 @@
 var osm = L.tileLayer(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-         maxZoom: 18,
-         attribution: 'Map data © \
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data © \
               <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-        });
+  });
+
+var StartLocation = L.latLng(55.650575, 12.541276) //The start of the journey - is later going to be changed to gps coordinates
+var EndLocation = L.latLng(55.678437, 12.572282)
 
 
 var mymap = L.map('map', {
-    center: [55.676111, 12.568333],
-    zoom: 10,
-    layers: [osm] // add it here
+  center: [55.676111, 12.568333],
+  zoom: 10,
+  layers: [osm] // add it here
 });
+
+
+
+var route = L.Routing.control({
+  waypoints: [
+    StartLocation,
+    EndLocation
+  ],
+  routeWhileDragging: true,
+  router: new L.Routing.openrouteservice('5b3ce3597851110001cf6248cc3ff0efc5c54f8591b049453e9138cf')
+}).addTo(mymap);
+
+var basemaps = {
+  "OpenStreetMap": osm
+}
+//var overlays = {"Route": control}
 
 L.control.scale().addTo(mymap);
 
@@ -25,14 +44,7 @@ L.control.scale().addTo(mymap);
 
 // StartCoordinates = null
 
-var basemaps = {"OpenStreetMap": osm}
 
-//L.easyButton('fa-globe', function(btn, map, locationEvent){
-
-//var overlays = {"Route": control}
-
-var StartLocation = L.latLng(55.650575, 12.541276) //The start of the journey - is later going to be changed to gps coordinates
-var EndLocation = L.latLng(55.678437, 12.572282)
 // mymap.on('locationfound', function (locationEvent) {
 // alert(locationEvent)
 //   var StartLocation = locationEvent//var Her = locationEvent
@@ -42,71 +54,3 @@ var EndLocation = L.latLng(55.678437, 12.572282)
 
 // mymap.locate({setView : true})
 // StartLocation = mymap.getCenter()
-
-
-// window.onload = function() { //When the window is opened it connects to our part of the server?
-//
-//   let orsDirections = new Openrouteservice.Directions({
-//     api_key: "5b3ce3597851110001cf6248cc3ff0efc5c54f8591b049453e9138cf"
-//   });
-//
-//   orsDirections.calculate({
-//     coordinates: [[8.690958, 49.404662], [8.687868, 49.390139]],
-//     profile: "driving-car",
-//     extra_info: ["waytype", "steepness"],
-//     geometry_format: "encodedpolyline",
-//     format: "json",
-//     mime_type: "application/json",
-//   })
-//     .then(function(json) {
-//         // Add your own result handling here
-//         console.log(JSON.stringify(json));
-//     })
-//     .catch(function(err) {
-//         console.error(err);
-//     });
-// };
-
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://api.openrouteservice.org/directions?api_key=5b3ce3597851110001cf6248cc3ff0efc5c54f8591b049453e9138cf&coordinates=8.34234,48.23424%7C8.34423,48.26424&profile=cycling-road&geometry_format=polyline');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {//ReadyState 4 = Done - So when it is done do the following steps
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-
-  }
-};
-
-request.send();
-
-// L.geoJSON(data, {
-//     style: function (feature) {
-//         return {color: feature.properties.color};
-//     }
-// }).bindPopup(function (layer) {
-//     return layer.feature.properties.description;
-// }).addTo(map);
-
-//
-//
-// Every thing below is the old routing engine - delete when the new one works
-//
-
-
-// var control = L.Routing.control({
-//   waypoints: [
-// StartLocation,
-// EndLocation
-//   ],
-//   router: new L.Routing.osrmv1({
-//     language: 'en',
-//     profile: 'bike', //Method of transport
-//     steps: 'true' //Adds a guide for the trip
-//   })//,
-// //  geocoder: L.Control.Geocoder.nominatim({}) This code I haven't activated yet, but it should help translating from addresses to latlon
-// })
-// //})
-// .addTo(mymap);
