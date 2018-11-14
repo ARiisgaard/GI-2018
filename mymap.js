5cfbd9f240b21b89397d21b20c81bb4202241257
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2018-11-14 11:49:07 +0100

    Doesn't work

--- mymap.js
+++ mymap.js
@@ -5,34 +5,40 @@ var osm = L.tileLayer(
               <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
   });
 
-var city = L.OWM.current({appId: 'ee67f8f53521d94193aa7d8364b7f5d9', intervall: 15, lang: 'en', showWindDirection: 'deg'});
+var city = L.OWM.current({
+  appId: 'ee67f8f53521d94193aa7d8364b7f5d9',
+  intervall: 15,
+  lang: 'en',
+  showWindDirection: 'deg'
+});
 
 var myIcon = L.icon({ //defines the icon for the wind location
-    iconUrl: 'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/256/Map-Marker-Marker-Outside-Chartreuse-icon.png', //Temporary, so we can see the difference between locations and stations
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [-3, -76]
+  iconUrl: 'http://icons.iconarchive.com/icons/icons-land/vista-map-markers/256/Map-Marker-Marker-Outside-Chartreuse-icon.png', //Temporary, so we can see the difference between locations and stations
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+  popupAnchor: [-3, -76]
 });
 var myLayer; //Layer with distination
 
 var stations = new L.GeoJSON.AJAX("stations.geojson", { //creating the "stations" layer
-onEachFeature: function(feature, layer, ) { //creating popup, when clicking on features.
-layer.bindPopup("<h2>Station:</h2>" + " " + feature.properties.navn + "<br>") //tells what to say in the popup. Has to use data from each feature depending on 'navn'.
+  onEachFeature: function(feature, layer, ) { //creating popup, when clicking on features.
+    layer.bindPopup("<h2>Station:</h2>" + " " + feature.properties.navn + "<br>") //tells what to say in the popup. Has to use data from each feature depending on 'navn'.
   }
 });
 
 stations.on('click', function(e) {
-    coords2 = [e.latlng.lat, e.latlng.lng];
-    });
+  coords2 = [e.latlng.lat, e.latlng.lng];
+});
 
-      var mymap = L.map('map', {
-        center: [55.676111, 12.568333],
-        zoom: 10,
-        layers: [osm, stations] // add it here
-      })
-    ;
+var mymap = L.map('map', {
+  center: [55.676111, 12.568333],
+  zoom: 10,
+  layers: [osm, stations] // add it here
+});
 
-var overlayMaps = {"Cities": city };//Adds the overlayer with weather information
+var overlayMaps = {
+  "Cities": city
+}; //Adds the overlayer with weather information
 
 var basemaps = {
   "OpenStreetMap": osm
@@ -49,77 +55,79 @@ mymap.locate({ //This is the code for the gps coordinates - it isn't
   })
   .on('locationfound', function(e) {
     var coords = L.latLng([e.latitude, e.longitude]);
-    var StartLocation = coords  //The start of the journey
+    var StartLocation = coords //The start of the journey
 
 
 
-var api_address = 'http://api.openweathermap.org/data/2.5/weather?lat='+e.latitude+'&lon='+e.longitude+'&appid=ee67f8f53521d94193aa7d8364b7f5d9'
-//var api_address = 'http://api.openweathermap.org/data/2.5/weather?lat=55.656553&lon=12.557593&appid=ee67f8f53521d94193aa7d8364b7f5d9'
+    var api_address = 'http://api.openweathermap.org/data/2.5/weather?lat=' + e.latitude + '&lon=' + e.longitude + '&appid=ee67f8f53521d94193aa7d8364b7f5d9'
+    //var api_address = 'http://api.openweathermap.org/data/2.5/weather?lat=55.656553&lon=12.557593&appid=ee67f8f53521d94193aa7d8364b7f5d9'
 
-fetch(api_address).then(response => {
-  return response.json();
-}).then(data => {
-  // Work with JSON data here
-  var windangle = data.wind.deg
+    fetch(api_address).then(response => {
+      return response.json();
+    }).then(data => {
+      // Work with JSON data here
+      var windangle = data.wind.deg
 
-var angle = windangle //The direction that the bicylclist is going to travel
+      var angle = windangle //The direction that the bicylclist is going to travel
 
-var length = 5000 //Distance traveled in meters
+      var length = 5000 //Distance traveled in meters
 
-//The following 10ish lines are defining the coordinates used to find the direction. The math behind it can be found here: http://www.movable-type.co.uk/scripts/latlong.html
+      //The following 10ish lines are defining the coordinates used to find the direction. The math behind it can be found here: http://www.movable-type.co.uk/scripts/latlong.html
 
-var StartLatInRat = e.latitude* Math.PI / 180
-var StartLngInRat = e.longitude* Math.PI / 180
-var AngleInRat = angle* Math.PI / 180
+      var StartLatInRat = e.latitude * Math.PI / 180
+      var StartLngInRat = e.longitude * Math.PI / 180
+      var AngleInRat = angle * Math.PI / 180
 
-var R = 6371e3; // Distance to the centre of the earth in metres
-var end_y = Math.asin( Math.sin(StartLatInRat)*Math.cos(length/R) +
-                    Math.cos(StartLatInRat)*Math.sin(length/R)*Math.cos(AngleInRat) );
-var end_x = StartLngInRat + Math.atan2(Math.sin(AngleInRat)*Math.sin(length/R)*Math.cos(StartLatInRat),
-                         Math.cos(length/R)-Math.sin(StartLatInRat)*Math.sin(end_y));
-var EndLat = end_y*180/Math.PI
-var EndLng = end_x*180/Math.PI
+      var R = 6371e3; // Distance to the centre of the earth in metres
+      var end_y = Math.asin(Math.sin(StartLatInRat) * Math.cos(length / R) +
+        Math.cos(StartLatInRat) * Math.sin(length / R) * Math.cos(AngleInRat));
+      var end_x = StartLngInRat + Math.atan2(Math.sin(AngleInRat) * Math.sin(length / R) * Math.cos(StartLatInRat),
+        Math.cos(length / R) - Math.sin(StartLatInRat) * Math.sin(end_y));
+      var EndLat = end_y * 180 / Math.PI
+      var EndLng = end_x * 180 / Math.PI
 
-//Here stops the coordinate definition
-
-
-var Winddestination = L.marker([EndLat, EndLng], {icon: myIcon}).addTo(mymap);
-var EndLocation = L.latLng(EndLat, EndLng) //This line defines the location of the destination - currently it is only defined by going in the direction with the least wind. Later it is going to be replaced with the station the closest to said location
+      //Here stops the coordinate definition
 
 
-//The next couple of lines are the code used to connect to server, that is attatched to the pgAdmin database
-//Since the code doesn't work at the moment it hasn't been properly documented yet - but basicly the it is the same code as we saw in the parking machine example with small changes
-  if(myLayer){
-      mymap.removeLayer(myLayer);
-  }
+      var Winddestination = L.marker([EndLat, EndLng], {
+        icon: myIcon
+      }).addTo(mymap);
 
-  var url = "http://127.0.0.1:5000/findstation?lat="+EndLat+"&lng="+EndLng
-    console.log(url)
 
-    $.getJSON(url, function(data){
+      //The next couple of lines are the code used to connect to server, that is attatched to the pgAdmin database
+      //Since the code doesn't work at the moment it hasn't been properly documented yet - but basicly the it is the same code as we saw in the parking machine example with small changes
+      if (myLayer) {
+        mymap.removeLayer(myLayer);
+      }
 
-      console.log(data)
+      var url = "http://127.0.0.1:5000/findstation?lat=" + EndLat + "&lng=" + EndLng
+      console.log(url)
 
-      // add GeoJSON layer to the map once the file is loaded
-      myLayer = L.geoJson(data).bindPopup(function (layer) {
-      return layer.feature.properties.navn;
+      $.getJSON(url, function(data) {
+
+        console.log(data)
+
+        // add GeoJSON layer to the map once the file is loaded
+        //     myLayer = L.geoJson(data).bindPopup(function (layer) {
+        //     return layer.feature.geometry.coordinates;
+        // })
+        //     myLayer.addTo(mymap);
+
+        var EndLocation = L.latLng(EndLat, EndLng) //This line defines the location of the destination - currently it is only defined by going in the direction with the least wind. Later it is going to be replaced with the station the closest to said location
+
+
+        //Here the routing begins
+        var route = L.Routing.control({
+          waypoints: [ //This defines from there the route should start and end
+            StartLocation,
+            EndLocation
+          ],
+          router: new L.Routing.openrouteservice('5b3ce3597851110001cf6248cc3ff0efc5c54f8591b049453e9138cf') //This line is telling the program that it should use ORS to calculate the route. The string is our personal api_key
+        }).addTo(mymap);
+      })
+    })
   })
-      myLayer.addTo(mymap);
-});
-
-//Here the routing begins
-    var route = L.Routing.control({
-      waypoints: [//This defines from there the route should start and end
-        StartLocation,
-        EndLocation
-      ],
-      router: new L.Routing.openrouteservice('5b3ce3597851110001cf6248cc3ff0efc5c54f8591b049453e9138cf') //This line is telling the program that it should use ORS to calculate the route. The string is our personal api_key
-    }).addTo(mymap);
-  }).catch(err => {
-    // Do something for an error here
-  });// End of finding the angle
-  })
-  .on('locationerror', function(e) {//This refers back to the gps part of the code - so it returns an error message if it cant get access to the gps - if that is the case it skips all of the other steps
+  .on('locationerror', function(e) { //This refers back to the gps part of the code - so it returns an error message if it cant get access to the gps - if that is the case it skips all of the other steps
     console.log(e);
     alert("Location access denied.");
   });
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+import password
 
 import psycopg2
 import json
-import sys
+#import sys
 
 
 app = Flask(__name__)
@@ -25,27 +26,27 @@ def findstation():
     conn = psycopg2.connect("dbname='Destinations' \
                              host='localhost' \
                              user='postgres' \
-                             password='hxs46xzp'")
+                             password='"+password.kode+"'")
 
     cur = conn.cursor() #What is the meaning of this?#
 
-    cur.execute("""SELECT *
+    cur.execute("""SELECT id, navn, navnekate0, ST_AsText(geom)
 FROM station
 ORDER BY ST_DISTANCE(ST_SetSRID(
 	ST_MakePoint(%s, %s), 4326)::geography, geom::geography)
 LIMIT 1;""", (lng, lat ))
 
     res = cur.fetchone()
+    print res
+    navn = res[1]
+    navnekate0 = res[2]
 
-    navn = res[0]
-    navnekate0 = res[1]
+    closest_station = json.dumps(res[3]) #What is the meaning of these numbers?
 
-    closest_station = json.dumps(res[2]) #What is the meaning of these numbers?
+#print(closest_station, file = sys.stderr)
+    closest_coords = closest_station.split("(")[1] ##[ removed
 
-print(closest_station, file=sys.stderr)
-    closest_coords = closest_station.split("[")[1] ##[ removed
-
-    coords = closest_coords.split("]")[0]##] removed
+    coords = closest_coords.split(")")[0]##] removed
 
     response = '{"type": "Feature","geometry": {"type": "Point","coordinates": ['+coords+']},"properties": {"vejnavn": "'+navn+'", "type": "'+navnekate0+'"}}'
 
