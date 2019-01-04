757e0ab521cd85e62990e000d337c513531cb3b5
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2019-01-04 10:01:55 +0100

    Made documentation for the flask server

--- server.py
+++ server.py
@@ -1,17 +1,14 @@
+#Here the used modules are imported
 from flask import Flask
 from flask import request
 from flask import Response
 from flask_cors import CORS
-import password
-
 import psycopg2
 import json
-import urllib2 #Kan slettes, hvis ikke den bruges
-#import sys
-
-# import configparser
 import requests
-# import sys
+
+#This is the name of the file, where the password for the database is stored. This was nessercery since we didn't have the same passwords
+import password
 
 
 app = Flask(__name__)
@@ -63,34 +60,36 @@ def findstation():
     conn = psycopg2.connect("dbname='Destinations' \
                              host='localhost' \
                              user='postgres' \
-                             password='"+password.kode+"'")
+                             password='"+password.kode+"'")#Connects to our database with stations
 
-    cur = conn.cursor() #What is the meaning of this?#
+    cur = conn.cursor()
 
     cur.execute("""SELECT id, navn, navnekate0, ST_AsText(geom)
 FROM station
 ORDER BY ST_DISTANCE(ST_SetSRID(
 	ST_MakePoint(%s, %s), 4326)::geography, geom::geography)
-LIMIT 1;""", (lng, lat ))
+LIMIT 1;""", (lng, lat ))#This is the request send to the database - sort all stations by distance from the "ideal destination" and return one value
 
-    res = cur.fetchone()
+    res = cur.fetchone() #Gets the one result from the database
     print(res)
-    navn = res[1]
-    navnekate0 = res[2]
+    navn = res[1] #This is the name of the station
+    navnekate0 = res[2] #This is the station-type
 
-    closest_station = json.dumps(res[3]) #What is the meaning of these numbers?
+    closest_station = json.dumps(res[3]) #This is the coordinates of the station converted to the json information
 
-#print(closest_station, file = sys.stderr)
-    closest_coords = closest_station.split("(")[1] ##[ removed
+    #Since pgadmin keeps its coordinates stored in a different way, than Leaflet is used to work with, we return the lat and long one by one
 
-    coords = closest_coords.split(")")[0]##] removed
+    closest_coords = closest_station.split("(")[1]
+
+    coords = closest_coords.split(")")[0]
     x = coords.split()[0]
     y = coords.split()[1]
 
+    #This is the response that is going to be send back to js
     json_response = '{"type": "Feature","geometry": {"type": "Point","coordinates": ['+x+','+y+']},"properties": {"vejnavn": "'+navn+'", "type": "'+navnekate0+'"}}'
 
     return Response(json_response, content_type='application/json; charset=UTF-8')
 
-
+    #This ends the connection to the database - otherwise it would keep running, and we would keep making new connections every time we look for a new station
     cur.close()
     conn.close()
